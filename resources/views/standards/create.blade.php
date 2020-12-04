a99fe5737c4a3aef61c783fc352a8ffcd5aa887d
Hardeep <hardeep01.technaitra@gmail.com>
2020-12-04 18:51:37 +0530

    standards crud

---
+++ resources/views/standards/create.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+@section('content')
+<!-- BEGIN: Content-->
+<div class="content-body">
+                <!--Form layout section start -->
+                <section id="basic-form-layouts">
+                    <div class="row match-height justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title" id="basic-layout-form">Add Standard</h4>
+                                    <a class="heading-elements-toggle">
+                                        <i class="la la-ellipsis-v font-medium-3"></i>
+                                    </a>
+                                    <div class="heading-elements">
+                                        <ul class="list-inline mb-0">
+                                            <li>
+                                                <a data-action="collapse">
+                                                    <i class="ft-minus"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a data-action="reload">
+                                                    <i class="ft-rotate-cw"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a data-action="expand">
+                                                    <i class="ft-maximize"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a data-action="close">
+                                                    <i class="ft-x"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="card-content collapse show">
+                                    <div class="card-body">
+                                        @if($errors->all())
+                                        <div class="alert alert-danger">
+                                            @foreach($errors->all() as $error)
+                                            
+                                               <p>{{$error}}</p> 
+                                        
+                                            @endforeach
+                                            </div>
+                                        @endif
+                                        <form class="form" action="{{ route('standards.store') }}" method="post">
+                                            @csrf  
+                                            <div class="form-body">
+                                                <div class="form-group">
+                                                    <label for="name">Name</label>
+                                                    <input type="text" id="name" class="form-control" placeholder="Name" name="name">
+                                                </div>
+                                            </div>
+                                            <div class="form-actions" style="text-align: center;">
+                                                <a href="{{ route('standards.index')}}" method="post" class="btn btn-primary" type="submit"> View all</a>    
+                                                <button type="submit" class="btn btn-success">Save
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('layouts.home');
+});
+
+Route::get('standards/create','StandardsController@create')->name('standards.create');
+Route::post('standards/store','StandardsController@store')->name('standards.store');
+Route::get('standards/index','StandardsController@index')->name('standards.index');
+Route::get('standards/edit/{id}','StandardsController@edit')->name('standards.edit');
+Route::patch('standards/update/{id}','StandardsController@update')->name('standards.update');
+Route::delete('standards/destroy/{id}','StandardsController@destroy')->name('standards.destroy');
+Route::post('standards/getstandard','StandardsController@getStandard')->name('get.standards');
+
+
+
+
+
+
+
+
 
