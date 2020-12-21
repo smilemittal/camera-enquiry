a6bba2fed2e61893eb72b938bdf76aa42a0e9037
abhinav.technaitra <abhinav.technaitra@gmail.com>
2020-12-21 13:48:25 +0530

    icons

--- resources/views/system_types/partials/setting-action.blade.php
+++ resources/views/system_types/partials/setting-action.blade.php
@@ -4,8 +4,8 @@
     </button>
 		<span aria-labelledby="btnSearchDrop12" class="dropdown-menu mt-1 dropdown-menu-right" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(55px, 27px, 0px);">
 		  	<a class="dropdown-item" href="{{ route('system-types.edit', [$system_type->id]) }}" >
-		 	<i class="fa fa-edit"></i>Edit</a>
-			 <a href="#" data-form_id="{{ 'delete_form_'.$system_type->id }}" class="delete dropdown-item" ><i class="fa fa-delete"></i>Delete</a>
+		 	<i class="ft-edit-2"></i>Edit</a>
+			 <a href="#" data-form_id="{{ 'delete_form_'.$system_type->id }}" class="delete dropdown-item" ><i class="ft-trash-2"></i>Delete</a>
 		        <form action="{{ route('system-types.destroy', [$system_type->id]) }}" method="post" id="{{ 'delete_form_'.$system_type->id }}">
 		           @csrf 
 		           @method('DELETE')
