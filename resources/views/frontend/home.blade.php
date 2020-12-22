f1f28786a49fa395afb78bd93b65a1622a86881a
navjot  singh <navjot.technaitra@gmail.com>
2020-12-22 11:46:34 +0530

    new css

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,4 +1,12 @@
 @extends('frontend.layouts.app')
+@section('styles')
+    <style>
+        #product-enquiry .active {
+            background: #007ecb !important;
+        }
+    </style>
+@endsection
+
 @section('content')
 <form method="post" action="" id="product-enquiry" enctype="multipart/form-data">
         <div class="form-sec">
