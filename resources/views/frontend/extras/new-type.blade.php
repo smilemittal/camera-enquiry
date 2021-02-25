0bf05f60c4fab254e2e35f803ac0775a5da3ccb6
Navjot <navjottechnaitra>
2021-02-25 17:35:23 +0530

    quantity empty bug

--- resources/views/frontend/extras/new-type.blade.php
+++ resources/views/frontend/extras/new-type.blade.php
@@ -1,5 +1,5 @@
 <div class="col-kemey section_{{ $type->slug }} {{ $type->slug . '_' . $i }}" data-type="{{ $type->slug }}">
-    <div class="row d-flex align-items-center">
+    <div class="row d-flex align-items-center helo">
         <div class="col-xl-3 col-md-6">
             <a class="btn" data-toggle="collapse" href="#{{ 'multiCollapseExample' . $type->slug . $i }}" role="button"
                 aria-expanded="false" aria-controls="multiCollapseExample1">{{ translate($type->name) }}</a>
